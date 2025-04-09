7948347efdc48c8c9d2df8566b48203487f91f87
farooq <ufservices465@gmail.com>
2025-04-09 07:10:46 +0530

    fix: remove merge conflict markers and clean up page component structure

--- app/events/technical/page.tsx
+++ app/events/technical/page.tsx
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-
-=======
-// import Competition from '@/components/main/CulturalCompetition'
-import Events from '@/components/main/Event'
-import Footer from '@/components/main/Footer'
-import Navbar from '@/components/main/Navbar'
-import StarsCanvas from '@/components/main/StarCanvas'
->>>>>>> 504b2f667cedce06829513e61812b1646ef59dba
 import Technical from '@/components/main/TechnicalCompetition'
 import React from 'react'
 
 function page() {
   return (
-    <div className="flex flex-col h-[950px] gap-20 bg-contain bg-fixed">
-      {/* <StarsCanvas/> */}
-      <Technical types='Technical Fest'/>
-    </div>
+    <><div className="flex flex-col h-[950px] gap-20 bg-contain bg-fixed">
+    {/* <StarsCanvas/> */}
+    <Technical types='Technical Fest'/>
+  </div></>
   )
 }
 
