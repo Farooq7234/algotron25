895fc137e795f9d0f5c2d69eba52d9c0d060e61f
farooq <ufservices465@gmail.com>
2025-04-20 12:10:30 +0530

    navigation register changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -68,7 +68,7 @@ const Hero = () => {
             initial="initial"
             animate="animate"
           >
-            <Link to="/sign-up">
+            <Link to="/signup">
               <motion.button
                 className="mt-6 cursor-pointer w-[12em] sm:w-[16em] md:w-[18em] h-[3em] text-white font-semibold text-sm sm:text-base md:text-lg bg-gradient-to-r from-violet-500 via-sky-500 to-pink-500 bg-[length:400%] rounded-[10px] z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:via-purple-500 before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[10px] before:hover:blur-lg before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:10%] active:bg-violet-700 focus:ring-violet-700"
                 style={{ zIndex: 0 }}
